841a89cf3e91c4488029481f1f065ec4e41aafe2
Jakub Motyka <jakub.motyka@feverup.com>
2025-06-26 15:32:35 +0200

    feat/update-branded-writer-txt-avoid-us-we-and-commercial-tone

--- app-branded.py
+++ app-branded.py
@@ -26,7 +26,7 @@ if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
 if not st.session_state.authenticated:
     pw = st.text_input(
-        "Enter your super-ultra secret password (v26/06/2025 15:27h)",
+        "Enter your super-ultra secret password (v26/06/2025 15:32h)",
         type="password"
     )
     if pw == PASSWORD:
